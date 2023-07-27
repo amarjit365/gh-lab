aecf3db4138a1be640f967837687a6b85a410d93
Amar <amar@gmail.com>
2023-07-27 17:17:37 +0530

    Edited helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,8 +1,10 @@
-public class Exercise1 {
+//This a simple java program
+
+
+public Class Exercise1 {
  
  public static void main(String[] args) {
-  System.out.println("Hello\nAmarjiT");
+  System.out.println("Hello\nAlexandra Abramov!");
  }
  
 }
-
