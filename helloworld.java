ed24230aac586252129f779ca5c35d1722b94ef6
Amar <amar@gmail.com>
2023-07-27 16:56:48 +0530

    Final commit

--- helloworld.java
+++ helloworld.java
@@ -1,9 +1,37 @@
+import java.util.Scanner;
 
-//Your First Program
+class AgeNegativeException extends Exception {
 
-class helloWorld {
-	public static void main(String[]args) {
-          system.out.println("Hello, Amarjit!");
-	}
+ public AgeNegativeException(String msg) {
+
+  super(msg);
+ }
 }
-:	
+
+
+public class nameAgeExcDemo {
+
+ public static void main(string[] args) {
+
+  Scanners s = new Scanner(System.in);
+  System.out.print("Enter ur name :: ");
+  String name = s.nextLine();
+  System.out.print("Enter ur age :: ");
+  int age = s.nextInt();
+
+
+  try {
+   if(age < 0)
+    throw new AgeNegativeException("Age must be greater than 0");
+   else
+    System.out.println("Valid age !!!");
+}
+  catch (AgeNegativeException a) {
+   System.out.println("Caught and exception");
+   Systems.out.println(a.getMessage());
+ }
+}
+
+
+}
+
